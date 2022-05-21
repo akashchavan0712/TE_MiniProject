1dbad661eb3f8b122195aab8bfafaf686600b682
akashchavan0712 <akashbchavan17@gmail.com>
2022-05-21 13:17:49 +0530

    first commit

---
+++ client/src/styles.js
@@ -0,0 +1,20 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    paddingLeft: '35px',
+    backgroundColor: '#000957',
+    paddingTop: "10px",
+    paddingBottom: "10px",
+  },
+  heading: {
+    color: '#fff',
+  },
+  image: {
+    marginLeft: '15px',
+  },
+}));
